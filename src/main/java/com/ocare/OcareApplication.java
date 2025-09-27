a63140b31f90ce660f67374523511e876697cdb9
sangminna <sm9171@nate.com>
2025-09-27 22:44:47 +0900

    Initial commit

---
+++ src/main/java/com/ocare/OcareApplication.java
@@ -0,0 +1,13 @@
+package com.ocare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocare/OcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
